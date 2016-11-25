cb3f2021b3d952dacf162c0564ce94df0c8b6827
Elana Hashman <elana.hashman@rackspace.com>
2016-11-25 16:23:32 -0500

    Add tests for getting LM devices

--- test/alertlogic_lib/core_test.clj
+++ test/alertlogic_lib/core_test.clj
@@ -1,5 +1,6 @@
 (ns alertlogic-lib.core-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.java.io :refer [resource]]
+            [clojure.test :refer :all]
             [cheshire.core :as json]
             [manifold.deferred :as md]
             [alertlogic-lib.core :as alc :refer :all]))
@@ -17,6 +18,11 @@
             output (#'alc/al-headers token)]
         (is (= expected output))))))
 
+(defn fake-get-success
+  [body]
+  (fn [_addr _headers]
+    (md/success-deferred {:body (json/generate-string body)})))
+
 (deftest get-page!-tests
   (testing "returns ::fetch-error on exception"
     (let [fake-get
@@ -26,9 +32,28 @@
         (is (= :alertlogic-lib.core/fetch-error
                @(get-page! "" ""))))))
   (testing "returns deserialized json body"
-    (let [fake-get
-          (fn [_addr _headers]
-            (md/success-deferred {:body (json/generate-string {:hosts []})}))]
+    (let [fake-get (fake-get-success {:hosts []})]
       (with-redefs [aleph.http/get fake-get]
         (is (= {:hosts []}
                @(get-page! "" "")))))))
+
+(deftest get-lm-devices!-tests
+  (testing "handles an empty device list"
+    (let [fake-get (fake-get-success {:hosts []})]
+      (with-redefs [aleph.http/get fake-get]
+        (is (= '()
+               (get-lm-devices-for-customer! "1111" "some-token"))))))
+  (testing "handles some devices"
+    (let [body (read-string (slurp (resource "test/hosts.edn")))
+          fake-get (fake-get-success body)]
+      (with-redefs [aleph.http/get fake-get]
+        (let [expected '({:name "123455"
+                          :type "appliance"
+                          :status "ok"
+                          :ips ["172.16.10.5"]}
+                         {:name "987654"
+                          :type "host"
+                          :status "offline"
+                          :ips ["10.15.0.1" "192.168.0.13"]})
+              output (get-lm-devices-for-customer! "1111" "some-token")]
+          (is (= expected output)))))))
