a7708639203a9d6d5e3f53fb356fb11885fe8b56
Elana Hashman <elana.hashman@rackspace.com>
2017-04-21 13:49:53 -0400

    Return deferreds and don't discard API data
    Fixes #14.
    
    Here I fix some bad design choices I initially made. We stop discarding
    useful API data, and we no longer dereference returned deferreds for the
    consumer.

--- test/alertlogic_lib/core_test.clj
+++ test/alertlogic_lib/core_test.clj
@@ -25,10 +25,12 @@
         (is (= {:hosts []}
                @(get-page! "" "")))))))
 
-(deftest get-customers!-tests
+(def customers
+  [{:customer-id 101 :customer-name "123-lol"}
+   {:customer-id 1111 :customer-name "746228 Ltd."}])
+
+(deftest customer-json-to-id-map-tests
   (let [cj->id #'alertlogic-lib.core/customer-json-to-id-map
-        customers [{:customer-id 101 :customer-name "123-lol"}
-                   {:customer-id 1111 :customer-name "746228 Ltd."}]
         customers-map {"123" "101"
                        "746228" "1111"}
         check-output (fn [input expected]
@@ -38,26 +40,31 @@
     (testing "handles bad customer names"
       (let [customer-data [{:customer-id 101 :customer-name "lol"}]]
         (check-output customer-data {})))
+    (testing "handles null customer names"
+      (let [customer-data [{:customer-id 101 :customer-name nil}]]
+        (check-output customer-data {})))
     (testing "handles good customer names"
       (let [customer-data [{:customer-id 101 :customer-name "123-lol"}]]
         (check-output customer-data {"123" "101"})))
     (testing "handles many customers"
-      (check-output customers customers-map))
-    (let [root-customer-data {:api-key "supar-sekret"
-                              :customer-id 31337
-                              :customer-name "SuperCustomer"
-                              :child-chain customers}
-          fake-get (fake-get-success root-customer-data)]
-      (with-redefs [aleph.http/get fake-get]
-        (testing "handles download"
-          (let [expected customers
-                output (get-customers! "31337" "supar-sekret")]
-            (is (= expected output))))
-        (testing "handles download and formatting"
-          (let [expected {"123" "101"
-                          "746228" "1111"}
-                output (get-customers-map! "31337" "supar-sekret")]
-            (is (= expected output))))))))
+      (check-output customers customers-map))))
+
+(deftest get-customers!-tests
+  (let [root-customer-data {:api-key "supar-sekret"
+                            :customer-id 31337
+                            :customer-name "SuperCustomer"
+                            :child-chain customers}
+        fake-get (fake-get-success root-customer-data)]
+    (with-redefs [aleph.http/get fake-get]
+      (testing "handles download"
+        (let [expected customers
+              output @(get-customers! "31337" "supar-sekret")]
+          (is (= expected output))))
+      (testing "handles download and formatting"
+        (let [expected {"123" "101"
+                        "746228" "1111"}
+              output (get-customers-map! "31337" "supar-sekret")]
+          (is (= expected output)))))))
 
 (defn use-atom-log-appender!
   "Adds a log observer that saves its log messages to an atom.
@@ -82,24 +89,20 @@
 (deftest get-lm-devices!-tests
   (testing "taps out when id is null"
     (let [log (use-atom-log-appender!)]
-      (get-lm-devices-for-customer! nil "some-token")
+      @(get-lm-devices-for-customer! nil "some-token")
       (is (= 1 (count @log)))
       (is (s/includes? (first @log) "Customer ID cannot be nil. Aborting."))))
   (testing "handles an empty device list"
     (let [fake-get (fake-get-success {:hosts []})]
       (with-redefs [aleph.http/get fake-get]
-        (is (empty? (get-lm-devices-for-customer! "1111" "some-token"))))))
+        (is (empty? @(get-lm-devices-for-customer! "1111" "some-token"))))))
   (testing "handles some devices"
-    (let [body (read-string (slurp (resource "test/hosts.edn")))
+    (let [body (-> "test/hosts.edn" resource slurp read-string)
           fake-get (fake-get-success body)]
       (with-redefs [aleph.http/get fake-get]
-        (let [expected '({:name "123455"
-                          :type "appliance"
-                          :status "ok"
-                          :ips ["172.16.10.5"]}
-                         {:name "987654"
-                          :type "host"
-                          :status "offline"
-                          :ips ["10.15.0.1" "192.168.0.13"]})
-              output (get-lm-devices-for-customer! "1111" "some-token")]
+        (let [expected (-> "test/processed-hosts.edn"
+                           resource
+                           slurp
+                           read-string)
+              output @(get-lm-devices-for-customer! "1111" "some-token")]
           (is (= expected output)))))))
