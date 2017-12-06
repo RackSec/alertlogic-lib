ec9aec1caf951de085c319d2c72fad78d2079704
Elana Hashman <elana.hashman@rackspace.com>
2017-12-06 13:07:22 -0500

    Bump dep versions

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [aleph "0.4.1"]
+                 [aleph "0.4.3"]
                  [camel-snake-kebab "0.4.0"]
-                 [cheshire "5.6.3"]
-                 [com.taoensso/timbre "4.7.4"]
-                 [manifold "0.1.5"]]
+                 [cheshire "5.8.0"]
+                 [com.taoensso/timbre "4.10.0"]
+                 [manifold "0.1.6"]]
   :plugins [[lein-cljfmt "0.5.6" :exclusions [com.google.javascript/closure-compiler
                                               org.clojure/clojurescript]]
             [jonase/eastwood "0.2.3"]
@@ -17,6 +17,6 @@
   :min-lein-version "2.7.0"
   :profiles {:uberjar {:aot :all}
              :repl {:main alertlogic-lib.core}
-             :dev {:dependencies [[pjstadig/humane-test-output "0.8.1"]]
+             :dev {:dependencies [[pjstadig/humane-test-output "0.8.3"]]
                    :injections [(require 'pjstadig.humane-test-output)
                                 (pjstadig.humane-test-output/activate!)]}})
